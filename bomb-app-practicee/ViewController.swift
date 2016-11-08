8529b7f9db6fc4b3efedf2fa45db3f1272ed62d4
Samer Mujibuddin <WorkStation@Samers-MacBook-Pro.local>
2016-11-08 11:20:44 -0600

    Added code for hide blue bomb and hide red bomb

--- bomb-app-practicee/ViewController.swift
+++ bomb-app-practicee/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+        
+    }
 
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        redBomb.hidden = true
+    }
 
 }
 
