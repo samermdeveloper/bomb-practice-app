f3114d446d246839a759ce933f26c682e9760fbd
Samer Mujibuddin <WorkStation@Samers-MacBook-Pro.local>
2016-11-08 11:08:20 -0600

    Initial Commit

---
+++ bomb-app-practiceeTests/bomb_app_practiceeTests.swift
@@ -0,0 +1,36 @@
+//
+//  bomb_app_practiceeTests.swift
+//  bomb-app-practiceeTests
+//
+//  Created by Samer Mujibuddin on 11/8/16.
+//  Copyright © 2016 Samer Mujibuddin. All rights reserved.
+//
+
+import XCTest
+@testable import bomb_app_practicee
+
+class bomb_app_practiceeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
